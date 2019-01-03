97981c7e2c6c8f5f7f9229db2acc447904a21d13
Craig Earley <cjearley13@earlham.edu>
2019-01-03 13:03:34 -0500

    adds image selection at random from directory

--- treetweets.py
+++ treetweets.py
@@ -1,8 +1,11 @@
 import tweepy
 import json
+import os, random, sys
 
 envroot = '/cluster/home/cjearley13/cooltreepix/'
 filepath = envroot + 'credentials.json'
+imagedir = envroot + "sources/"
+tweeteddir = envroot + "tweeted/"
 
 with open(filepath) as data_file:
     data = json.load(data_file)
@@ -17,9 +20,19 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-image = "IMAG0245.jpg"
-imagePath = envroot + image
 status = ""
 
+if not os.listdir(imagedir):
+	print("empty directory, get more pictures")
+	sys.exit()
+
+currentimage = random.choice(os.listdir(imagedir))
+
+imagepath = imagedir + currentimage
+
 # Send the tweet.
-api.update_with_media(imagePath, status)
+api.update_with_media(imagepath, status)
+
+# store the tweeted image so it isn't repeated
+tweetedimagepath = tweeteddir + currentimage
+#os.rename(selectedimagepath,tweetedimagepath)
