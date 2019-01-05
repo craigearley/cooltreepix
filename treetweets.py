42e394ca4bb2e583992a3094ba657b29f1521a95
Craig Earley <cjearley13@earlham.edu>
2019-01-05 13:11:04 -0500

    guarantees float as percentage, adds debug mode

--- treetweets.py
+++ treetweets.py
@@ -1,6 +1,9 @@
 import tweepy
 import json
 import os, random, sys, subprocess
+import argparse
+
+DEBUG = False
 
 # a file of up to 3072KB can be uploaded to Twitter, max size is rounded down (unit is bytes)
 MAXFILESIZE=3000000
@@ -17,6 +20,11 @@ consumer_key = data['consumer_key']
 consumer_secret =  data['consumer_secret']
 access_token = data['access_token']
 access_token_secret = data['access_token_secret']
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--debug", help="debug mode", action="store_true")
+args = parser.parse_args()
+DEBUG = args.debug
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -35,9 +43,16 @@ imagepath = imagedir + currentimage
 
 imagesize = os.path.getsize(imagepath)
 if (imagesize > MAXFILESIZE):
-	percentage = MAXFILESIZE / imagesize * 100
+	percentage = float(MAXFILESIZE) / float(imagesize) * 100.0
 	resize_command = '/usr/bin/convert -resize ' + str(percentage) + '% ' + imagepath + ' ' + imagepath
+	if (DEBUG):
+		print(imagepath + " is too large, trying to resize...")
+		print(resize_command)
 	subprocess.call(resize_command, shell=True)
+
+# everything after this should only happen as part of production
+if (DEBUG):
+	sys.exit()
 
 # Send the tweet.
 api.update_with_media(imagepath, status)
